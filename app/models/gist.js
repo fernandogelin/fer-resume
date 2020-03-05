9362855e868ddd48b9bec22f0a7396089d304bfa
fernandogelin <fernando@fernandogelin.com>
2020-03-05 12:08:31 -0500

    fix: 🐛 add id to resume model

--- app/models/gist.js
+++ app/models/gist.js
@@ -2,5 +2,6 @@ import Model, { attr, belongsTo } from "@ember-data/model";
 
 export default class GistModel extends Model {
   @attr("string") url;
+  @attr("string") locale;
   @belongsTo("resume") resume;
 }
