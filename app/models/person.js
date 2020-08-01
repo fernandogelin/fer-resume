a7308c7a7f6c0c18208908b4cf5f2aebc2bc86df
fernandogelin <fernando@fernandogelin.com>
2020-08-01 09:57:50 -0400

    feat: add phonetic name, update contend

--- app/models/person.js
+++ app/models/person.js
@@ -8,6 +8,7 @@ export default class PersonModel extends Model {
   @attr('string') phone
   @attr('string') website
   @attr('string') summary
+  @attr('string') phonetic_name
   @hasMany('profile') profiles
   @belongsTo('location') location
   @belongsTo('resume') resume
